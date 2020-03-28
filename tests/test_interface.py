c27d08f2e9329e1d20a56d743abacde9dd27e407
LiNaK <k.sasa6ra.0317@gmail.com>
2020-03-28 09:09:58 +0900

    Revert "FirstSetting"
    This reverts commit f29ca11971695009fb6abb9b347b4950dff75794.

--- tests/test_interface.py
+++ tests/test_interface.py
@@ -9,11 +9,16 @@ This is the test for interface classes which has specific abstract method.
 
 # Standard module
 import unittest
+from unittest.mock import patch
 
 # 3rd party's module
 
 # Original module  
 from context import src # path setting
+from testing_utility.unittest_util import cls_startstop_msg as add_msg
+
+from src.interface.intfc_com import (Transaction, Reader, Writer, PathAsk, 
+                                     ValueAsk, TF_Maker)
 
 class TestForMethodExist():
     '''
@@ -25,23 +30,22 @@ class TestForMethodExist():
     _class_attr_pairs = (())
     
     def test_class_method_pairs(self):
-        istype =lambda cl,m: self.assertTrue(callable(getattr(cl,m)))
-        self.check_sth_in_cls(self._class_method_pairs, istype)
+        for cl, mth in self._class_method_pairs:
+            self.assertTrue(callable(getattr(cl, mth)))
     
     def test_class_attr_pairs(self):
-        istype =lambda cl,a: self.assertTrue(hasattr(cl,a))
-        self.check_sth_in_cls(self._class_attr_pairs, istype)
-    
-    def check_sth_in_cls(self, cls_sth_pairs, istype):
-        for cl, sth in cls_sth_pairs:
-            if isinstance(sth, list) or isinstance(sth, tuple):
-                for s in sth:
-                    istype(cl, s)
-            else:
-                istype(cl, sth)
-        pass
-    
-    
+        for cl, attr in self._class_attr_pairs:
+            self.assertTrue(hasattr(cl, attr))
+
+@add_msg
+class TestCombinePN(TestForMethodExist, unittest.TestCase):
+    _class_method_pairs=((Transaction,'execute'),
+                         (Reader,'read'),
+                         (Writer,'write'),
+                         (PathAsk,'get_path'),
+                         (ValueAsk, 'get_value'),
+                         (TF_Maker, 'get_tf')
+                         )
 
 if __name__=='__main__':
     unittest.main()
